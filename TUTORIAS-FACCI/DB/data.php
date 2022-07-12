3edb53ce4d093f718af632a53f4ed0e4e7888bca
@espinales1309@gmail.com <esalvatierra89@hotmail.com>
2022-07-12 09:37:54 -0500

    eliminación

--- TUTORIAS-FACCI/DB/data.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Slim\Http\Request;
-use Slim\Http\Response;
-
-//login
-$app->get('/[{name}]', function (Request $request, Response $response, array $arg){
-    $this->logger->info("Slim-Skeleton '/'")
-})
