79bad3c963139ce23bf7067fea759c3b6c167169
Emiliafc17 <108153621+Emiliafc17@users.noreply.github.com>
2022-07-24 23:25:09 -0500

    Update actividadesdocnt.js

--- TUTORIAS-FACCI/JS/actividadesdocnt.js
+++ TUTORIAS-FACCI/JS/actividadesdocnt.js
@@ -1,62 +1,46 @@
-const formulario = document.getElementById('formulario');
-const inputs = document.querySelectorAll('#formulario input');
+const form = document.getElementById('formulario')
+const submit = document.getElementById('button-send')
 
-const expresiones = {
-    tarea: /^[a-zA-ZÀ-ÿ\s]{1,225}$/,
-	correcion: /^[a-zA-ZÀ-ÿ\s]{1,225}$/,
-    observaciones:/^[a-zA-Z0-9\_\-]{1,100}$/, 
 
-}
-const campos = {
+const tarea = document.getElementById('tarea')
+const correccion = document.getElementById('correccion')
+const observaciones = document.getElementById('observaciones')
+const fecha = document.getElementById('date')
+const select = document.getElementById('select')
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    validateForm()
+})
+
+const formIsValid ={
 	tarea: false,
-    correcion: false,
-    select: false,
-    date: false,
-    observaciones: false,
+	correccion: false,
+	observaciones: false,
+	fecha: false,
+	select: false
 }
 
-const validarFormulario = (e) => {
-	switch (e.target.name) {
-        case "tema":
-            validarCampo(expresiones.tema, e.target, 'tema');
-        break;
-		case "horas":
-			validarCampo(expresiones.horas, e.target, 'horas');
-		break;
-		case "tareas":
-			validarCampo(expresiones.tareas, e.target, 'tareas');
-		break;
-        case "observaciones":
-			validarCampo(expresiones.observaciones, e.target, 'observaciones');
-		break;
-	}
+tarea.addEventListener('change', (e) => {
+    if(e.target.value.trim().length > 0) formIsValid.tarea = true
+})
+correcion.addEventListener('change', (e) => {
+    if(e.target.value.trim().length > 0) formIsValid.correcion = true
+})
+observaciones.addEventListener('change', (e) => {
+    if(e.target.value.trim().length > 0) formIsValid.observaciones = true
+})
+fecha.addEventListener('change', (e) => {
+    if(e.target.value.trim().length > 0) formIsValid.fecha = true
+})
+select.addEventListener('change', (e) => {
+    if(e.target.value.trim().length > 0) formIsValid.select = true
+})
+
+const validateForm = () => {
+    const formValues = Object.values(formIsValid)
+    const valid = formValues.findIndex(value => value == false)
+    if(valid == -1) form.submit()
+    else alert('Form invalid')
 }
-
-const validarCampo = (expresion, input, campo) => {
-	if(expresion.test(input.value)){
-		document.getElementById(`grupo__${campo}`).classList.remove('content-form-incorrecto');
-		document.getElementById(`grupo__${campo}`).classList.add('content-form-correcto');
-		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
-		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
-		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
-		campos[campo] = true;
-	} else {
-		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
-		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
-		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
-		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
-		campos[campo] = false;
-	}
-} 
-
-inputs.forEach((input) => {
-	input.addEventListener('keyup', validarFormulario);
-	input.addEventListener('blur', validarFormulario);
-});
-
-formulario.addEventListener('submit', (e) => {
-	e.preventDefault();
-
-
-});
