1171cb357d822d31d09bff6d5019e74e8ea3c36c
Emiliafc17 <108153621+Emiliafc17@users.noreply.github.com>
2022-07-27 03:03:17 -0500

    Update tutoriadocnt.js

--- TUTORIAS-FACCI/JS/tutoriadocnt.js
+++ TUTORIAS-FACCI/JS/tutoriadocnt.js
@@ -1,90 +1,44 @@
-const formulario = document.getElementById('formulario');
-const inputs = document.querySelectorAll('#formulario input');
-
-const expresiones = {
-    tema: /^[a-zA-ZÀ-ÿ\s]{1,225}$/,
-	horas: /^[a-zA-Z0-9\_\-]{4,16}$/, 
-	tareas: /^[a-zA-ZÀ-ÿ\s]{1,225}$/, 
-    observaciones:/^[a-zA-Z0-9\_\-]{1,100}$/, 
-
-}
-
-const campos = {
-	tema: false,
-	horas: false,
-    tareas: false,
-    group: false,
-    date: false,
-	proxdate: false,
-    observaciones: false,
-
-}
-
-const validarFormulario = (e) => {
-	switch (e.target.name) {
-        case "tema":
-            validarCampo(expresiones.tema, e.target, 'tema');
-        break;
-		case "horas":
-			validarCampo(expresiones.horas, e.target, 'horas');
-		break;
-		case "tareas":
-			validarCampo(expresiones.tareas, e.target, 'tareas');
-		break;
-        case "observaciones":
-			validarCampo(expresiones.observaciones, e.target, 'observaciones');
-		break;
-	}
-}
-
-const validarCampo = (expresion, input, campo) => {
-	if(expresion.test(input.value)){
-		document.getElementById(`grupo__${campo}`).classList.remove('content-form-incorrecto');
-		document.getElementById(`grupo__${campo}`).classList.add('content-form-correcto');
-		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
-		campos[campo] = true;
-	} else {
-		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
-		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
-		campos[campo] = false;
-	}
-}
-
-inputs.forEach((input) => {
-	input.addEventListener('keyup', validarFormulario);
-	input.addEventListener('blur', validarFormulario);
-});
-
-formulario.addEventListener('submit', (e) => {
-	e.preventDefault();
-});
-
-
-$inputs.forEach((input) => {
-    input.addEventListener("keyup", validarFormulario);
-    input.addEventListener("blur", validarFormulario);
-});
-
-
-
-
-$formulario.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const $terminos = document.getElementById("button-send");
-    if(campos.tema && campos.horas && campos.tareas && campos.group && campos.date && $terminos.proxdate) {
-        // formulario.reset();
-
-        document.getElementById("formulario__mensaje-exito").classList.add("formulario__mensaje-exito-activo");
-        setTimeout(() => {
-        }, 3000);
-    
-        setTimeout(() => {
-            location.reload();
-        }, 5000);
-
-    } else {
-
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("formulario").addEventListener('submit', validarFormulario); 
+  });
+  
+  function validarFormulario(evento) {
+    evento.preventDefault();
+    var tema = document.getElementById('tema').value;
+    if(tema.length == 0) {
+		alert('No has escrito nada en tema');
+      return;
     }
-});
+    var horas = document.getElementById('horas').value;
+    if(horas.length == 0) {
+		alert('No has escrito nada en horas');
+      return;
+    }
+    var tareas = document.getElementById('tareas').value;
+    if(tareas.length == 0) {
+		alert('No has escrito nada en tareas');
+      return;
+    }
+	var select = document.getElementById('select').value;
+    if(select.length == 0) {
+		alert('el grupo no es valido');
+      return;
+    }
+    var date = document.getElementById('date').value;
+    if(date.length == 0) {
+		alert('la fecha no es valida');
+      return;
+    }
+	var proxdate = document.getElementById('proxdate').value;
+    if(proxdate.length == 0) {
+		alert('la proxima fecha no es valida');
+      return;
+    }
+	var observaciones = document.getElementById('observaciones').value;
+    if(observaciones.length == 0) {
+		alert('No has escrito nada en observaciones');
+      return;
+    }
+ 
+    this.submit();
+  }
